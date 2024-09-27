6a7f544969dcc947dc1345e57c3b119dd05dbf9a
daivid-1 <daivid.aba@gmail.com>
2024-09-27 17:31:45 +0300

    fix the route component

--- frontend/src/routeRoots.tsx
+++ frontend/src/routeRoots.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TableFieldInput from "./components/FieldInput";
 import { HomePage } from "./pages/home/homePage";
 import { UserPage } from "./pages/users/userPage";
-import { Route, Routes, useLocation } from "react-router-dom";
-import TableFieldInput from "./components/FieldInput";
 
 export const RoutingRoot: React.FC = () => {
-  // const location = useLocation();
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/a" element={<UserPage />} />
-      <Route path="/b" element={<TableFieldInput />} />
+      <Route path="/" element={<HomePage />}>
+        <Route path="b" element={<TableFieldInput />} />
+      </Route>
+
+      <Route path="/user/*" element={<UserPage />}>
+        <Route path="a" element={<UserPage />} />
+      </Route>
     </Routes>
   );
 };
